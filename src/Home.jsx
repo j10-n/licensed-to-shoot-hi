baa7c682362028c13db5c2069da57893ba98f319
justen <justen@justens-mbp.ksbe.edu>
2019-11-26 13:38:16 -1000

    hello world shows on screen

---
+++ src/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Hello World</p>
+      </div>
+    );
+  }
+}
+
+export default Home;
