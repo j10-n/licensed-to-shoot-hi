932b0c2927649ae13f569ab3ea42eb35942f6f2b
justen <justen@justens-mbp.ksbe.edu>
2019-11-27 10:11:13 -1000

    created basic homepage layout with horizontal list

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,7 +4,25 @@ class Home extends React.Component {
   render() {
     return (
       <div>
-        <p>Hello World</p>
+        <h1>LICENSED TO SHOOT HAWAII</h1>
+
+        <li className="menu">
+          <ul className="first">
+            <a href="#">About</a>
+          </ul>
+          <ul>
+            <a href="#">Athletes</a>
+          </ul>
+          <ul>
+            <a href="#">Videos</a>
+          </ul>
+          <ul>
+            <a href="#">Schools</a>
+          </ul>
+          <ul className="last">
+            <a href="#">Contact</a>
+          </ul>
+        </li>
       </div>
     );
   }
